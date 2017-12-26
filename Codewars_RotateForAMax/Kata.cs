1263682230f9039d5f9d6f9a78506903c6006d54
sqz777In91 <sqz777isme@gmail.com>
2017-12-26 11:50:30 +0800

    fail test case : input 123

--- Codewars_RotateForAMax/Kata.cs
+++ Codewars_RotateForAMax/Kata.cs
@@ -1,9 +1,18 @@
+using System.Collections.Generic;
+
 namespace Codewars_RotateForAMax
 {
     public class Kata
     {
         public static long MaxRot(long n)
         {
+            if (n.ToString().Length < 2)
+                return n;
+            var nString = n.ToString();
+            var nChar0 = nString[1];
+            var roatedN = int.Parse(nChar0 + nString.Substring(0, 1));
+            if (roatedN > n)
+                return roatedN;
             return n;
         }
     }
--- Codewars_RotateForAMax/UnitTest1.cs
+++ Codewars_RotateForAMax/UnitTest1.cs
@@ -10,5 +10,17 @@ namespace Codewars_RotateForAMax
         {
             Assert.AreEqual(1,Kata.MaxRot(1));   
         }
+
+        [TestMethod]
+        public void Input_12_Should_Be_21()
+        {
+            Assert.AreEqual(21,Kata.MaxRot(12));
+        }
+
+        [TestMethod]
+        public void Input_123_Should_Be_231()
+        {
+            Assert.AreEqual(231,Kata.MaxRot(123));
+        }
     }
 }
